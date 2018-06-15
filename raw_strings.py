40979701a5123960121233d0a16a5330b141bb46
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-06-15 21:28:25 +0300

    Update raw_strings.py

--- raw_strings.py
+++ raw_strings.py
@@ -1,3 +1,14 @@
+# File: raw_strings.py
+# Description: Example on how to show raw strings in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Example on how to show raw strings in Python // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Raw_strings_in_Python (date of access: XX.XX.XXXX)
+
 
 # Raw strings
 x = 'Hello\nworld!'  # This is not a raw string
