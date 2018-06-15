a05aae528ca2b0b3f3601fc384a8114c3d067c36
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-06-15 21:27:37 +0300

    Add files via upload

---
+++ raw_strings.py
@@ -0,0 +1,8 @@
+
+# Raw strings
+x = 'Hello\nworld!'  # This is not a raw string
+y = 'Hello\'world!'  # This is not a raw string
+z = r'Hellow\nworld'  # This is a raw string
+print(x)
+print(y)
+print(z)
